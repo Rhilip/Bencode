62d74fa2b9663a1a43cfce3e9285f11a177112ef
Rhilip <rhilipruan@gmail.com>
2020-02-27 17:50:25 +0800

    test: Add PHPUnit test
    1. Add phpunit as dev require, Remove require of ext-ctype
    2. Add PHPUnit test For Bencode::decode() and Bencode::encode()
    3. Add more ParseErrorException
    
    Closes #1

--- src/Bencode.php
+++ src/Bencode.php
@@ -13,8 +13,8 @@ namespace Rhilip\Bencode;
  *
  * Convert Map:
  *  - Dictionary (starts with d, ends with e, with key sort)
- *  - List (starts with l, ends with e
- *  - Integer (starts with i, ends with e
+ *  - List (starts with l, ends with e)
+ *  - Integer (starts with i, ends with e)
  *  - String (starts with number denoting number of characters followed by : and then the string)
  *
  * @package Rhilip\Bencode
@@ -36,7 +36,11 @@ class Bencode
      */
     public static function decode($data, &$pos = 0)
     {
-        $start_decode = ($pos === 0);
+        $start_decode = ($pos === 0);   // If it is the root call ?
+        if ($start_decode && (!is_string($data) || strlen($data) == 0)) {
+            throw new ParseErrorException('Decode Input is not valid String.');
+        }
+
         if ($data[$pos] === 'd') {
             $pos++;
             $return = [];
@@ -47,7 +51,9 @@ class Bencode
                     break;
                 }
                 if (!is_string($key)) {
-                    throw new ParseErrorException('Invalid key type, must be string: ' . gettype($key));
+                    throw new ParseErrorException('Non string key found in the dictionary.');
+                } elseif (array_key_exists($key, $return)) {
+                    throw new ParseErrorException('Duplicate Dictionary key exist before: ' . $key);
                 }
                 $return[$key] = $value;
             }
@@ -64,33 +70,47 @@ class Bencode
         } elseif ($data[$pos] === 'i') {
             $pos++;
             $digits = strpos($data, 'e', $pos) - $pos;
-            $return = substr($data, $pos, $digits);
-            if ($return === '-0') {
-                throw new ParseErrorException('Cannot have integer value -0');
-            }
-            $multiplier = 1;
-            if ($return[0] === '-') {
-                $multiplier = -1;
-                $return = substr($return, 1);
-            }
-            if (!ctype_digit($return)) {
-                throw new ParseErrorException('Cannot have non-digit values in integer number: ' . $return);
-            }
-            $return = $multiplier * ((int)$return);
+            $value = substr($data, $pos, $digits);
+            $return = self::checkInteger($value);
             $pos += $digits + 1;
         } else {
             $digits = strpos($data, ':', $pos) - $pos;
-            $len = (int)substr($data, $pos, $digits);
+            $len = self::checkInteger(substr($data, $pos, $digits));
+            if ($len < 0) {
+                throw new ParseErrorException('Cannot have non-digit values for String length');
+            }
+
             $pos += ($digits + 1);
             $return = substr($data, $pos, $len);
+
+            if (strlen($return) != $len) {  // Check for String length is match or not
+                throw new ParseErrorException('String length is not match for: ' . $return . ', want ' . $len);
+            }
+
             $pos += $len;
         }
-        if ($start_decode) {
-            if ($pos !== strlen($data)) {
-                throw new ParseErrorException('Could not fully decode bencode string');
-            }
+
+        if ($start_decode && $pos !== strlen($data)) {
+            throw new ParseErrorException('Could not fully decode bencode string');
         }
         return $return;
+    }
+
+    /**
+     * This private function help us filter value like `-13` `13` will pass the filter and return it's int value
+     * Other value like ``,`-0`, `013`, `-013`, `2.127`, `six` will throw A ParseErrorException
+     *
+     * @param string $value
+     * @return int
+     * @throws ParseErrorException
+     */
+    private static function checkInteger($value)
+    {
+        $int = intval($value);
+        if (strval($int) !== $value) {
+            throw new ParseErrorException('Invalid integer format or integer overflow: ' . $value);
+        }
+        return $int;
     }
 
     /**
