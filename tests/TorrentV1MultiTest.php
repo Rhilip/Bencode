3087dbd3ca83edb6714fdf27d9efc920aaa89014
Rhilip <rhilipruan@gmail.com>
2022-03-27 22:47:36 +0800

    test: add base tests for vaild torrent

---
+++ tests/TorrentV1MultiTest.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once 'traits\TorrentFileCommonTrait.php';
+
+use Rhilip\Bencode\TorrentFile;
+use PHPUnit\Framework\TestCase;
+
+class TorrentV1MultiTest extends TestCase
+{
+    use TorrentFileCommonTrait;
+
+    protected $protocol = TorrentFile::PROTOCOL_V1;
+    protected $fileMode = TorrentFile::FILEMODE_MULTI;
+
+    protected $infoHashs = [
+        TorrentFile::PROTOCOL_V1 => '344f85b35113783a34bb22ba7661fa26f1046bd1',
+        TorrentFile::PROTOCOL_V2 => null
+    ];
+}
